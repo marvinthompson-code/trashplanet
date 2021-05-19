d2428e5b35ebddc7d70b7a50fd4b18f40175fda0
Marvin <marvinthompson@pursuit.org>
2021-05-18 21:36:04 -0400

    setting up and testing cart functions

--- trashplanet/src/features/Cart/Cart.js
+++ trashplanet/src/features/Cart/Cart.js
@@ -6,18 +6,31 @@ const Cart = () => {
   const [cart, setCart] = useState([]);
   const [cartId, setCartId] = useState(null);
   const [cartItems, setCartItems] = useState([]);
+  const [subTotal, setSubtotal] = useState("");
+  const [totalItems, setTotalItems] = useState(0);
 
   // cart functions
   const refreshCart = () => {
     const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
     commerce.cart.refresh().then((cart) => setCart(cart));
   };
-  const emptyCart = () => {
-    const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
-    // use a debugger to figure out what the response is, and do stuff with it. Maybe make a popup?
-    commerce.cart.empty().then((response) => console.log(response));
+
+  const handleCartClear = () => {
+    const emptyCart = () => {
+      const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
+      // use a debugger to figure out what the response is, and do stuff with it. Maybe make a popup?
+      commerce.cart.empty().then((response) => console.log(response));
+    };
+    try {
+      emptyCart();
+      debugger;
+      refreshCart();
+    } catch (error) {
+      console.log(error.message);
+    }
   };
 
+  //maybe save this function for later? once an item is fully bought, then update cart
   const updateCart = (itemId, newQuantity) => {
     // The update() method uses PUT v1/carts/{cart_id}/items/{line_item_id} to update the quantity or variant for the line item ID in the cart.
     const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
@@ -27,14 +40,6 @@ const Cart = () => {
       .then((response) => console.log(response));
   };
 
-//   const addToCart = (itemId, quantity) => {
-//     const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
-//     commerce.cart
-//       .add(itemId, quantity)
-//       //do stuff with response
-//       .then((response) => console.log(response));
-//   };
-
   const getCartId = () => {
     const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
     commerce.cart.id().then((cartId) => setCartId(cartId));
@@ -42,14 +47,42 @@ const Cart = () => {
 
   const getCartItems = () => {
     const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
-    commerce.cart.contents().then((items) => setCartItems(items));
+    commerce.cart.contents().then((items) => {
+      debugger;
+      setCartItems(items);
+    });
   };
 
   useEffect(() => {
-    const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
-    commerce.cart.retrieve().then((cart) => setCart(cart));
+    const retriveCart = () => {
+      try {
+        // create a new updated instance of the cart
+        const commerce = new Commerce(`${REACT_APP_CHEC_PUBLIC_KEY}`);
+        commerce.cart.retrieve().then((cart) => {
+          debugger;
+          // extracting pieces of important data from our cart
+          let { total_items, id } = cart;
+          let { formatted_with_symbol } = cart.subtotal;
+          setTotalItems(total_items);
+          setSubtotal(formatted_with_symbol);
+          setCartId(id);
+          setCart(cart);
+        });
+
+        // then get the items so we can display them??
+        getCartItems();
+        debugger;
+      } catch (error) {
+        console.log(error.message);
+      }
+    };
+    retriveCart();
   }, []);
-  return <div></div>;
+  return (<div>
+
+  </div>);
 };
 
 export default Cart;
+
+// {!totalItems ? <h1>Your cart is empty</h1> : null}
